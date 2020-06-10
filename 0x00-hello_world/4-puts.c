6ce824e5d394990b3098507dda4586540ce5a1eb
JHS1790 <1790@holbertonschool.com>
2020-06-10 18:47:58 +0000

    0x00. C - Hello, World|Task 4. Hello, puts|Bugfixed script, removed char string = and placed directly in puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,7 @@
 int main(voiud)
 {
 
-	char string[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(string);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 
