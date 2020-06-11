b1e44d8fe3cd77aae14a71d99591d27f1e94edf5
JHS1790 <1790@holbertonschool.com>
2020-06-11 16:49:46 +0000

    0x01. C - Variables, if, else, while|Task 3. alphABET|Created script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - declares n as positive or negative or 0
+ *
+ *
+ * Return: Ends the program
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
