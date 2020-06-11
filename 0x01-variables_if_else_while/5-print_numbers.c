9f20d175d9efa20b65bced43764c0766ff0df3c1
JHS1790 <1790@holbertonschool.com>
2020-06-11 17:31:02 +0000

    0x01. C - Variables, if, else, while|Task 5. Numbers|Created script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - declares n as positive or negative or 0
+ *
+ *
+ * Return: Ends the program
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
