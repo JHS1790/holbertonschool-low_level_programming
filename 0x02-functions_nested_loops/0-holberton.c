8adc76da9c5c9b6298d770b507181d1ac4cf098f
JHS1790 <1790@holbertonschool.com>
2020-06-15 16:35:09 +0000

    0x02. C - Functions, nested loops|Task 0. _putchar|Created script

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * main - does a thing
+ *
+ * Return: ends the program
+ *
+ *
+ */
+
+int main(void)
+{
+
+	char H = 72, o = 111, l = 108, b = 98, e = 101, r = 114, t = 116;
+	char n = 110, newline = 10;
+
+	_putchar(H);
+	_putchar(o);
+	_putchar(l);
+	_putchar(b);
+	_putchar(e);
+	_putchar(r);
+	_putchar(t);
+	_putchar(o);
+	_putchar(n);
+	_putchar(newline);
+
+	return (0);
+
+}
