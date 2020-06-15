605bd0650f442cdae511ef1077e769f6ccb96415
JHS1790 <1790@holbertonschool.com>
2020-06-15 20:25:42 +0000

    0x02. C - Functions, nested loops|Task 4. isalpha|Created script

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -84,5 +84,22 @@ int _islower(int c)
 		return (2);
 }
 
+/**
+ * _isalpha - returns if c is a letter or not
+ *
+ * @c: 1 is a letter, 0 is not
+ *
+ * Return: Ends program
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
+
 
 #endif /* HOLBERTON_H*/
