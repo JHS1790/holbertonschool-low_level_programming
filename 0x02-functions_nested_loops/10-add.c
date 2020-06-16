42f933da51ae2d2323ccd82b16eedfb21862d14d
JHS1790 <1790@holbertonschool.com>
2020-06-16 15:23:59 +0000

    0x02. C - Functions, nested loops|Task 10. a + b|Created script

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,21 +1,13 @@
 #include "holberton.h"
 /**
- * times_table - ends my sanity with the click of a button!
+ * add - ends my sanity with the click of a button!
+ * @a: first number to add
+ * @b: second number to add
  * Return: void
  */
-void times_table(void)
+int add(int a, int b)
 {
-	int h = 0, v = 0, a = 0, t = 0;
+	int c = a + b;
 
-	while (v <= 10)
-	{
-		while (h <= 10)
-		{
-			t = t + a;
-			_putchar(t);
-			h++;
-		}
-		h = 0;
-		v++;
-	}
+	return (c);
 }
