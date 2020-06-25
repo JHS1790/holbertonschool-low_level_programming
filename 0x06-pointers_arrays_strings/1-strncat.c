08b2412fa3ceed8f9f439bac3c3d60001ad92a36
JHS1790 <1790@holbertonschool.com>
2020-06-25 14:51:09 +0000

    0x06. C - More pointers, arrays and strings|Task 1. strncat|Created script

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * _strncat - cats two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: lengths of source string to use
+ * Return: new pointer to string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int ld = 0, ls = 0, w = 1;
+
+	if (*dest == 0 || *src == 0)
+		return (0);
+	while (*dest)
+	{
+		dest++;
+		ld++;
+	}
+	while (*src)
+	{
+		src++;
+		ls++;
+	}
+	src = src - ls;
+	while (w <= n && w <= ls)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		w++;
+	}
+	if (n <= ls)
+		dest = dest - (n + ld);
+	else
+		dest = dest - (ls + ld);
+	return (dest);
+}
