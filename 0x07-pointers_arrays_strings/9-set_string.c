325bb01fa3621ac941e9fce8f7f7e0ef89c4cca3
JHS1790 <1790@holbertonschool.com>
2020-06-30 15:25:48 +0000

    Im stupid

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * set_string - sets the string
  * @s: string to be written over
@@ -7,21 +6,5 @@
  */
 void set_string(char **s, char *to)
 {
-	char *b, *c = to;
-	int tlen = 0, slen = 0;
-
-	printf("hello world");
-	b = *s;
-	while (*c)
-	{
-		printf("%d pass through to", tlen);
-		*b = *c;
-		c++;
-		b++;
-		tlen++;
-		slen++;
-	}
-	*b = 0;
-	b = b - slen;
-	c = c - tlen;
+	*s = to;
 }
