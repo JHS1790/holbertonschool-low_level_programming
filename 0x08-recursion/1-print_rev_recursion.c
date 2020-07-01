3acf5d5e150c4a8211dadcb58fab358505174b5d
JHS1790 <1790@holbertonschool.com>
2020-07-01 14:56:35 +0000

    0x08. C - Recursion|Task 1. Why is it so important to dream? Because, in my dreams we are together|Completed task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints reverse string
+ * @s: input string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
