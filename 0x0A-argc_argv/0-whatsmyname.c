1fe2289ac380713d3ce0023d632980bf83e8e0b0
JHS1790 <1790@holbertonschool.com>
2020-07-05 15:03:58 +0000

    0x0A. C - argc, argv|0. It aint what they call you, its what you answer to|Created directory, README, and completed task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints name of function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: program exit
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
