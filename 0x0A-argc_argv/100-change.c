42aeb1559587a953cd8ebcafe42d08c6ab6436dd
JHS1790 <1790@holbertonschool.com>
2020-07-06 18:51:50 +0000

    special task complete

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,16 +12,23 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error");
+		printf("Error\n");
 		return(1);
 	}
 	change = atoi(argv[1]);
+	if (change < 0)
+	{
+		printf("0");
+		return(0);
+	}
 	coins += change / 25;
 	change %= 25;
 	coins += change / 10;
 	change %= 10;
 	coins += change / 5;
 	change %= 5;
+	coins += change / 2;
+	change %= 2;
 	coins += change / 1;
 	change %= 1;
 	printf("%d\n", coins);
