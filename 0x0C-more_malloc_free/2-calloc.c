46dc27328bd9f5ea044b758f6ed92a3fde2c580b
JHS1790 <1790@holbertonschool.com>
2020-07-09 20:09:35 +0000

    Task 2. Barrel shoot 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+void *_memset(void *s, unsigned int n);
 /**
  * _calloc - allocates memory for array
  * @nmemb: number of elements of array
@@ -10,12 +11,34 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(nmemb * size);
 	if (!array)
 		return (NULL);
+	array = _memset(array, (nmemb * size));
 	return (array);
+}
+
+/**
+ * _memset - sets part of array
+ * @s: array to be set
+ * @b: char to set
+ * @n: size
+ * Return: set array
+ */
+void *_memset(void *s, unsigned int n)
+{
+	char *t = s;
+	unsigned int l;
+
+	if (!s || !n)
+		return (0);
+	for (l = 0; l < n; l++)
+	{
+		*t = 0;
+		t++;
+	}
+	return (s);
 }
