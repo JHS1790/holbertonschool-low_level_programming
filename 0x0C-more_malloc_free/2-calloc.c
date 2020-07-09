f29d8e33f0a53f64b01bed517a5097ab61069a38
JHS1790 <1790@holbertonschool.com>
2020-07-09 19:58:23 +0000

    Task 2: barrel shoot 1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,15 +9,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *array;
-	int i;
+	void *array;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(nmemb * size);
 	if (!array)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
-		array[i] = 0;
 	return (array);
 }
