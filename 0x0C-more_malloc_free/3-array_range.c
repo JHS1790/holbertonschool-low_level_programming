a45faf64d4bcbd2bfbbd1b7c4dc392da1b847a43
JHS1790 <1790@holbertonschool.com>
2020-07-09 17:42:52 +0000

    Task 3: moved +1 into bracket in malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,13 +13,11 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	array = malloc(sizeof(int) * (max - min) + 1);
+	array = malloc(sizeof(int) * (max - min + 1));
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < (max - min); i++)
-	{
 		array[i] = min + i;
-	}
 	array[i] = max;
 	return (array);
 }
