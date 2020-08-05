641c2eaf51a312751b5cdbe39c17cbe566eb9f60
JHS1790 <1790@holbertonschool.com>
2020-08-05 16:04:40 +0000

    Task 3: finished out all the checks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,38 +7,50 @@
  */
 int main(int ac, char **av)
 {
-	int fd1, fd2, check;
+	int fd1, fd2, check, writecheck;
 	char buffer[1024];
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		return (98);
 	}
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 < 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		return (99);
 	}
 	while ((check = read(fd1, buffer, 1024)) > 0)
-		write(fd2, buffer, check);
+	{
+		if (check < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			return (98);
+		}
+		writecheck = write(fd2, buffer, check);
+		if (writecheck < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			return (99);
+		}
+	}
 	check = close(fd1);
 	if (check < 0)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", fd1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1);
 		return (100);
 	}
 	check = close(fd2);
 	if (check < 0)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
 		return (100);
 	}
 	return (0);
