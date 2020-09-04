a263d9e071162193125d6c42551573130a4363e0
JHS1790 <1790@holbertonschool.com>
2020-09-04 15:32:40 +0000

    First draft on task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: header for list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+        const dlistint_t *nav = h;
+        int count = 0;
+
+        while (nav)
+        {
+                count++;
+                nav = nav->next;
+        }
+        return (count);
+}
