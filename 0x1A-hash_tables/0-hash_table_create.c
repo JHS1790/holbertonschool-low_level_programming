3912b15f882399b6ef0f8955ed2b02de20b6a792
JHS1790 <1790@holbertonschool.com>
2020-10-02 15:21:13 +0000

    Task 0 mod. 2: bettyd and added a check for size = 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	for (i = 0; i < size; i++)
