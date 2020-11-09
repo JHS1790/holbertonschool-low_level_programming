a1638b888a7a895ce03dfb9fcdbf57283dcdf088
JHS1790 <1790@holbertonschool.com>
2020-11-09 15:56:12 +0000

    changed tabs for spaces

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,26 +3,26 @@
 
 def island_perimeter(grid):
     """H E L P (spelled in rocks)"""
-	island = []
-	perimeter = 0
-	x = 0
-	i = 0
-	while x < len(grid):
-		y = 0
-		while y < len(grid[x]):
-			if grid[x][y] == 1:
-				print("yay")
-				island.append((x, y))
-			y += 1
-		x += 1
-	while i < len(island):
-		if (island[i][0] + 1, island[i][1]) not in island:
-			perimeter += 1
-		if (island[i][0] - 1, island[i][1]) not in island:
-			perimeter += 1
-		if (island[i][0], island[i][1] + 1) not in island:
-			perimeter += 1
-		if (island[i][0], island[i][1] - 1) not in island:
-			perimeter += 1
-		i += 1
-	return perimeter
+    island = []
+    perimeter = 0
+    x = 0
+    i = 0
+    while x < len(grid):
+        y = 0
+        while y < len(grid[x]):
+            if grid[x][y] == 1:
+                print("yay")
+                island.append((x, y))
+            y += 1
+        x += 1
+    while i < len(island):
+        if (island[i][0] + 1, island[i][1]) not in island:
+            perimeter += 1
+        if (island[i][0] - 1, island[i][1]) not in island:
+            perimeter += 1
+        if (island[i][0], island[i][1] + 1) not in island:
+            perimeter += 1
+        if (island[i][0], island[i][1] - 1) not in island:
+            perimeter += 1
+        i += 1
+    return perimeter
