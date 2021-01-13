928a66ec00247e22b638b70fc522450015e3a1f9
JHS1790 <1790@holbertonschool.com>
2021-01-13 16:50:55 +0000

    first go at 0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+#ifndef ALGO
+#define ALGO
+
+/* Includes */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Structs */
+
+/* Project Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+/* Non-Project Prototypes */
+
+#endif /* ALGO */
